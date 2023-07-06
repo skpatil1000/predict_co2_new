e74aee079135d1a4ab94ad265b58c757e13585c2
skpatil1000 <skpatil1000@gmail.com>
2023-07-06 23:37:11 +0530

    modified config file

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 port_number = 8080
-model_file_path = r"static\models\linear_regression.pkl"
-json_file_path = r"static\json_files\project_data.json"
+model_file_path = "static/models/linear_regression.pkl"
+json_file_path = "static/json_files/project_data.json"
